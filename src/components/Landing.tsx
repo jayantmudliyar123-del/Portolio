5d18533d34a3318580805a83dd5bd87cc6f262a7
jayantmudliyar123-del <jayantmudliyar123@gmail.com>
2026-03-12 17:04:07 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              RAJESH
+              Jayant
               <br />
-              <span>CHITYAL</span>
+              <span>Mudliyar</span>
             </h1>
           </div>
           <div className="landing-info">
