dbc8b60dfddbebf659e30aec2d7e0b7ca764c92f
jayantmudliyar123-del <jayantmudliyar123@gmail.com>
2026-03-16 15:20:02 +0530

    Update SocialIcons.tsx

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -59,18 +59,14 @@ const SocialIcons = () => {
   return (
     <div className="icons-section">
       <div className="social-icons" data-cursor="icons" id="social">
+       
         <span>
-          <a href="https://github.com/rajeshchityal" target="_blank">
-            <FaGithub />
-          </a>
-        </span>
-        <span>
-          <a href="https://www.linkedin.com/in/rajeshchityal" target="_blank">
+          <a href="https://www.linkedin.com/in/jayanttmuudliyar" target="_blank">
             <FaLinkedinIn />
           </a>
         </span>
         <span>
-          <a href="https://x.com/rajeshchityal" target="_blank">
+          <a href="https://x.com/jayant17690" target="_blank">
             <FaXTwitter />
           </a>
         </span>
