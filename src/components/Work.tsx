63ddd9259eb6b35fd2e8ad787209661fc8fa6e8b
jayantmudliyar123-del <jayantmudliyar123@gmail.com>
2026-03-16 17:23:57 +0530

    Update Work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -4,36 +4,32 @@ import WorkImage from "./WorkImage";
 import { MdArrowBack, MdArrowForward } from "react-icons/md";
 
 const projects = [
-  {
-    title: "Solid Starters",
-    category: "Low-Code Platform",
-    tools: "Angular, Next.js, NestJS, MongoDB",
-    image: "/images/Solidx.png",
+    {
+    title: "Storyboard",
+    category: "Podcast platform",
+    tools: "Figma, Illustrator",
+    image: "/images/Storyboard.jpg",
   },
   {
-    title: "Radix",
-    category: "E-Commerce",
-    tools: "Angular, Next.js, NestJS, CMS",
-    image: "/images/radix.png",
+    title: "My Ticket Partner",
+    category: "Event Ticketing Platform for web",
+    tools: "Figma, Illustrator",
+    image: "/images/MTP2.jpg",
   },
   {
-    title: "Bond Cancellation",
-    category: "Import-Export Automation",
-    tools: "Angular, Next.js, NestJS, Workflows",
-    image: "/images/bond.png",
+    title: "My Ticket Partner",
+    category: "Event Ticketing Platform for mobile",
+    tools: "Figma, Illustrator",
+    image: "/images/MTP1.jpg",
   },
   {
-    title: "Sapphire",
-    category: "CRM Platform",
-    tools: "AngularJS, NestJS, PostgreSQL",
-    image: "/images/sapphire.png",
+    title: "My Costume Partner",
+    category: "Costume Selling platform",
+    tools: "Figma, Illustrator",
+    image: "/images/MCP.jpg",
   },
-  {
-    title: "Mpro",
-    category: "Insurance Platform",
-    tools: "React.js, Node.js, Microservices",
-    image: "/images/Maxlife.png",
-  },
+  
+ 
 ];
 
 const Work = () => {
