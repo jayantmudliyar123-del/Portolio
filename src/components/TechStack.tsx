5ef5f8c0360abb2909272ca3e1f2e64bb4478309
jayantmudliyar123-del <jayantmudliyar123@gmail.com>
2026-03-16 13:28:33 +0530

    Update TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,7 +13,7 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
+  "/images/photoshop.webp",
   "/images/next2.webp",
   "/images/node2.webp",
   "/images/express.webp",
