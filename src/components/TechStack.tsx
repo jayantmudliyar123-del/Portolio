4700453ab2efa0c9cd60586019b9c20f8a4b5809
jayantmudliyar123-del <jayantmudliyar123@gmail.com>
2026-03-16 13:30:02 +0530

    Update TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -14,13 +14,13 @@ import {
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
   "/images/photoshop.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/photoshop.webp",
+  "/images/photoshop.webp",
+  "/images/photoshop.webp",
+  "/images/photoshop.webp",
+  "/images/photoshop.webp",
+  "/images/photoshop.webp",
+  "/images/photoshop.webp",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
